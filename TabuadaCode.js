d2204ec3ca5d96349ad136e4255a863c0267bbc4
Arthur Lopes Oliveira <129803691+Arthurestak@users.noreply.github.com>
2024-05-15 15:24:46 -0300

    Create TabuadaCode.js

---
+++ TabuadaCode.js
@@ -0,0 +1,8 @@
+const num = 45;
+
+if (num == 0) {
+  console.log('O número digitado não tem tabuada!')
+};
+
+console.log(num1,',', num*2,',', num*3,',', num*4,',', num*5,',', num*6,',', num*7,',', num*8,',', num*9,',', num*10);
+
